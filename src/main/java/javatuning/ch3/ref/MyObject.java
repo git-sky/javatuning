af6dec40033c1a7becab2376fdb1a8384da80e41
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-07-30 16:51:01 +0800

    format

--- src/main/java/javatuning/ch3/ref/MyObject.java
+++ src/main/java/javatuning/ch3/ref/MyObject.java
@@ -1,13 +1,16 @@
 package javatuning.ch3.ref;
 
 public class MyObject {
-	@Override
-	protected void finalize() throws Throwable {
-		super.finalize();
-		System.out.println("MyObject's finalize called");
-	}
-	@Override
-	public String toString(){
-		return "I am MyObject";
-	}
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("MyObject's finalize called");
+    }
+
+    @Override
+    public String toString() {
+        return "I am MyObject";
+    }
+
 }
