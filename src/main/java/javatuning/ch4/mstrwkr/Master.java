a95190985331cca4dfc254cacd30081ae7b832d7
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-08-14 14:20:27 +0800

    ch4 mstrwkr

--- src/main/java/javatuning/ch4/mstrwkr/Master.java
+++ src/main/java/javatuning/ch4/mstrwkr/Master.java
@@ -7,11 +7,14 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Master {
-
+    /** 任务队列 */
     protected Queue<Object> workQueue = new ConcurrentLinkedQueue<Object>();
+    /** worker进程队列 */
     protected Map<String, Thread> threadMap = new HashMap<String, Thread>();
+    /** 子任务处理结果集 */
     protected Map<String, Object> resultMap = new ConcurrentHashMap<String, Object>();
 
+    /** 是否所有的子任务都结束了 */
     public boolean isComplete() {
         for (Map.Entry<String, Thread> entry : threadMap.entrySet()) {
             if (entry.getValue().getState() != Thread.State.TERMINATED) {
@@ -21,6 +24,7 @@ public class Master {
         return true;
     }
 
+    /** 构造函数,需要一个worker进程逻辑,和需要的worker进程数量 */
     public Master(Worker worker, int countWorker) {
         worker.setWorkQueue(workQueue);
         worker.setResultMap(resultMap);
@@ -28,14 +32,17 @@ public class Master {
             threadMap.put(Integer.toString(i), new Thread(worker, Integer.toString(i)));
     }
 
+    /** 提交一个任务 */
     public void submit(Object job) {
         workQueue.add(job);
     }
 
+    /** 返回子任务结果集 */
     public Map<String, Object> getResultMap() {
         return resultMap;
     }
 
+    /** 开始运行所有的worker进程,进行处理 */
     public void execute() {
         for (Map.Entry<String, Thread> entry : threadMap.entrySet()) {
             entry.getValue().start();
--- src/main/java/javatuning/ch4/mstrwkr/TestMasterWorker.java
+++ src/main/java/javatuning/ch4/mstrwkr/TestMasterWorker.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class TestMasterWorker {
 
     public class PlusWorker extends Worker {
+        @Override
         public Object handle(Object input) {
             Integer i = (Integer) input;
             return i * i * i;
@@ -16,12 +17,15 @@ public class TestMasterWorker {
 
     @Test
     public void testMasterWorker() {
+        //固定使用5个worker,并指定worker
         Master m = new Master(new PlusWorker(), 5);
-        for (int i = 0; i < 100; i++)
-            m.submit(i);
+        for (int i = 0; i < 100; i++) m.submit(i); //提交100个子任务
+        //开始计算
         m.execute();
+        //最终计算结果保存在此
         int re = 0;
         Map<String, Object> resultMap = m.getResultMap();
+        //不需要等待所有worker都执行完,即可开始计算最终结果
         while (resultMap.size() > 0 || !m.isComplete()) {
             Set<String> keys = resultMap.keySet();
             String key = null;
@@ -30,12 +34,9 @@ public class TestMasterWorker {
                 break;
             }
             Integer i = null;
-            if (key != null)
-                i = (Integer) resultMap.get(key);
-            if (i != null)
-                re += i;
-            if (key != null)
-                resultMap.remove(key);
+            if (key != null) i = (Integer) resultMap.get(key);
+            if (i != null) re += i;                 //最终结果
+            if (key != null) resultMap.remove(key); //移除已经被计算过的项
         }
 
         System.out.println("testMasterWorker:" + re);
