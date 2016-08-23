cc8f83d0d1591e28cce1b62bd2b12b3ddea8088e
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-08-23 13:46:45 +0800

    ch4 reentrantLock

--- src/main/java/javatuning/ch4/reentrantLock/ReentrantLockingTest.java
+++ src/main/java/javatuning/ch4/reentrantLock/ReentrantLockingTest.java
@@ -2,23 +2,14 @@ package javatuning.ch4.reentrantLock;
 
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockingTest {
 
-    final Lock lock = new ReentrantLock();
+    private final Lock lock = new ReentrantLock();
 
-    @Test
-    public void test() throws InterruptedException {
-        Thread first = new Thread(createTask(), "FirstThread");
-        Thread second = new Thread(createTask(), "SecondThread");
-        first.start();
-        second.start();
-        Thread.sleep(600);
-        second.interrupt();
-        Thread.sleep(10000);
-    }
 
     private Runnable createTask() {
         return new Runnable() {
@@ -26,10 +17,10 @@ public class ReentrantLockingTest {
             public void run() {
                 while (true) {
                     try {
-                        //if (lock.tryLock(500, TimeUnit.MILLISECONDS))
+                        if (lock.tryLock(500, TimeUnit.MILLISECONDS))
                         //if (lock.tryLock())
                         //lock.lock();
-                        lock.lockInterruptibly();
+                        //lock.lockInterruptibly();
                         {
                             try {
                                 System.out.println("locked " + Thread.currentThread().getName());
@@ -39,16 +30,30 @@ public class ReentrantLockingTest {
                                 System.out.println("unlocked  " + Thread.currentThread().getName());
                             }
                             break;
+                        } else { //这段代码在lock()和lockInterruptibly时删除
+                            System.out.println("unable to lock " + Thread.currentThread().getName());
                         }
-                        //else {
-                        //    System.out.println("unable to lock "+ Thread.currentThread().getName());
-                        //}
                     } catch (InterruptedException e) {
                         System.out.println(Thread.currentThread().getName() + " is Interrupted");
                     }
                 }
             }
         };
+    }
+
+
+    @Test
+    public void test() throws InterruptedException {
+        Thread first = new Thread(createTask(), "FirstThread");
+        Thread second = new Thread(createTask(), "SecondThread");
+
+        first.start();
+        second.start();
+
+        Thread.sleep(600);
+
+        second.interrupt(); //中断
+        Thread.sleep(10000);
     }
 
 }
