7ebe6571597ebeabfc5843eb16d51157867e7294
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-08-24 10:26:43 +0800

    ch4 atomic

--- src/main/java/javatuning/ch4/atomic/TestAtomic.java
+++ src/main/java/javatuning/ch4/atomic/TestAtomic.java
@@ -11,6 +11,7 @@ public class TestAtomic {
     private static final int MAX_THREADS = 3;
     private static final int TASK_COUNT = 3;
     private static final int TARGET_COUNT = 1000000;
+
     private AtomicInteger acount = new AtomicInteger(0);
     private int count = 0;
 
@@ -22,14 +23,14 @@ public class TestAtomic {
         return count;
     }
 
-    public class SyncThread implements Runnable {
-        protected String name;
-        protected long starttime;
+    public class SyncTask implements Runnable {
+        private String name;
+        private long startTime;
         TestAtomic out;
 
-        public SyncThread(TestAtomic o, long starttime) {
+        public SyncTask(TestAtomic o, long startTime) {
             out = o;
-            this.starttime = starttime;
+            this.startTime = startTime;
         }
 
         @Override
@@ -38,17 +39,17 @@ public class TestAtomic {
             while (v < TARGET_COUNT) {
                 v = out.inc();
             }
-            long endtime = System.currentTimeMillis();
-            System.out.println("SyncThread spend:" + (endtime - starttime) + "ms" + " v=" + v);
+            long endTime = System.currentTimeMillis();
+            System.out.println("SyncTask spend:" + (endTime - startTime) + "ms" + " v=" + v);
         }
     }
 
-    public class AtomicThread implements Runnable {
-        protected String name;
-        protected long starttime;
+    public class AtomicTask implements Runnable {
+        private String name;
+        private long startTime;
 
-        public AtomicThread(long starttime) {
-            this.starttime = starttime;
+        public AtomicTask(long startTime) {
+            this.startTime = startTime;
         }
 
         @Override
@@ -57,27 +58,27 @@ public class TestAtomic {
             while (v < TARGET_COUNT) {
                 v = acount.incrementAndGet();
             }
-            long endtime = System.currentTimeMillis();
-            System.out.println("AtomicThread spend:" + (endtime - starttime) + "ms" + " v=" + v);
+            long endTime = System.currentTimeMillis();
+            System.out.println("AtomicTask spend:" + (endTime - startTime) + "ms" + " v=" + v);
         }
     }
 
     @Test
     public void testAtomic() throws InterruptedException {
         ExecutorService exe = Executors.newFixedThreadPool(MAX_THREADS);
-        long starttime = System.currentTimeMillis();
-        AtomicThread atomic = new AtomicThread(starttime);
+        long startTime = System.currentTimeMillis();
+        AtomicTask atomic = new AtomicTask(startTime);
         for (int i = 0; i < TASK_COUNT; i++) {
             exe.submit(atomic);
         }
         Thread.sleep(10000);
     }
 
-    //@Test
+    @Test
     public void testSync() throws InterruptedException {
         ExecutorService exe = Executors.newFixedThreadPool(MAX_THREADS);
-        long starttime = System.currentTimeMillis();
-        SyncThread sync = new SyncThread(this, starttime);
+        long startTime = System.currentTimeMillis();
+        SyncTask sync = new SyncTask(this, startTime);
         for (int i = 0; i < TASK_COUNT; i++) {
             exe.submit(sync);
         }
